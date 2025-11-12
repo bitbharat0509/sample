82b33c120b6f35c9da2d092e5e19de040a4b1b0e
bitbharat0509 <bharatkdlyadav@gmail.com>
2025-11-12 12:30:49 +0530

    Implement number comparison program
    Add a program to compare two numbers and print the greater one.

--- new.cpp
+++ new.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+  int t;
+cin>>t;
+while(t--){
+int a,b;
+cin>>a>>b;
+if(a>b) cout<<a<<" is greater then "<<b<<endl;
+else cout<<b<<" is greater then "<<a<<endl;}} 
